e90edfc02fd5b5665f37440c174de15b2711e510
BachmannSolutions <BachmannSolutions@gmail.com>
2019-03-17 13:33:56 -0400

    finished learning about cool features when utilizing lists

--- CSharpCollections/Program.cs
+++ CSharpCollections/Program.cs
@@ -10,6 +10,23 @@ namespace CSharpCollections
     {
         static void Main(string[] args)
         {
-        }
+            HashSet<Student> students = new HashSet<Student>
+            {
+                new Student() { Name = "Sally", GradeLevel = 3},
+                new Student() { Name = "Bob", GradeLevel = 3},
+                new Student() { Name = "Sally", GradeLevel = 2}
+            };
+
+            Student joe = new Student() { Name = "Joe", GradeLevel = 2 };
+            students.Add(joe);
+
+            Student duplicateJoe = new Student() { Name = "Joe", GradeLevel = 2 };
+            students.Add(duplicateJoe);
+
+            foreach (Student student in students)
+            {
+                Console.WriteLine($"{student.Name} is in grade { student.GradeLevel}");
+            }
+        }   
     }
 }
